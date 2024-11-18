6416a2665b060b8f674d34f61f01ffaca8201746
devyadav-00 <dev428845@gmail.com>
2024-11-18 15:01:18 +0530

    removed comments

--- backend/middlewares/tpoAuth.js
+++ backend/middlewares/tpoAuth.js
@@ -1,6 +1,5 @@
 export const authorizeRoles = (...roles) => {
     return (req, res, next) => {
-      // `req.user.role` should be populated by the authentication middleware
       if (!roles.includes(req.user.role)) {
         return res.status(403).json({
           success: false,
